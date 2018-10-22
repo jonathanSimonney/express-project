247993f08022f129a1d47143c192ecad927bedc6
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2018-10-22 13:02:24 +0200

    began header templating

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ const PORT = 3000
 const HOST = 'localhost'
 
 app.set('view engine', 'pug')
-app.get('/', (req, res) => res.render('index'))
 app.use(express.static('public'))
+
+app.get('/', (req, res) => res.render('index'))
+app.get('/article/:id', (req, res) => res.render('article.pug'))
+app.get('/admin', (req, res) => res.render('admin/admin.pug'))
+app.get('/admin/write', (req, res) => res.render('admin/write.pug'))
+app.get('/admin/edit/:id', (req, res) => res.render('admin/edit.pug'))
+app.get('/admin/delete/:id', (req, res) => {
+    //todo
+})
+
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
