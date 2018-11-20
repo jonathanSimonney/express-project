bef247b33a4f05cd5753ebe2d118b6412bf97138
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2018-11-20 09:46:31 +0100

    admin home page done.

--- admin.router.js
+++ admin.router.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const adminRouter = express.Router()
+const Article = require('./models/Article.model')
 
-adminRouter.get('/', (req, res) => res.render('admin/admin.pug'))
+adminRouter.get('/', (req, res) => {
+	Article.find().populate('author category').exec().then(articles => {
+		res.render('admin/admin.pug', { articles })
+	}).catch(error => res.send(error.message))
+})
 adminRouter.get('/write', (req, res) => res.render('admin/write.pug'))
 adminRouter.get('/edit/:id', (req, res) => res.render('admin/edit.pug'))
 adminRouter.get('/delete/:id', (req, res) => {
