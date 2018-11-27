aa37d86b4ce6c40b4929f1d62705d8d749e6923c
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2018-11-27 19:23:35 +0100

    edit form displayed properly.

--- admin.router.js
+++ admin.router.js
@@ -43,16 +43,25 @@ adminRouter.get('/edit/:id', (req, res) => {
 	let displayErrorMessage = currentSession.valid === false;
 	req.session.valid = null;
 
-	Promise
-		.all([
-			Author.find().sort('name'),
-			Category.find().sort('title')
-		])
-		.then(([authors, categories]) => res.render('admin/edit', { authors, categories, displayErrorMessage }))
-		.catch((err) => {
-			currentSession.valid = false;
-			res.redirect(req.baseUrl + '/')
-		})
+	Article.findById(req.params.id).populate('author category').exec().then(article => {
+		if (article == null){//there isn't any article with given id, so we return a 404 error
+			res.status(404)
+			const resourceName = "article"
+			const resourceId = "id " + req.params.id
+			res.render('errors/404', {resourceName, resourceId})
+		}else{
+			Promise
+				.all([
+					Author.find().sort('name'),
+					Category.find().sort('title')
+				])
+				.then(([authors, categories]) => res.render('admin/edit', { article, authors, categories, displayErrorMessage }))
+				.catch((err) => {
+					currentSession.valid = false;
+					res.redirect(req.baseUrl + '/edit/' + req.params.id)
+				})
+		}
+	}).catch(error => res.send(error.message))
 });
 adminRouter.get('/delete/:id', (req, res) => {
     //todo
